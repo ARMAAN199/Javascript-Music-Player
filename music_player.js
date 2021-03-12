928da99ef1011cd486f6d7ba0b35dcb2346ee6b7
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-03-12 15:14:13 +0530

    commit message

--- music_player.js
+++ music_player.js
@@ -24,3 +24,57 @@ function clean_queue_part2() {
   music.pause();
   handlePlay();
 }
+
+function chooseartist(artist_name_from_above) {
+  if (artist_name_from_above == "chainsmoker_songs") {
+    clean_queue_part1();
+    songs = chainsmokers_songs;
+    clean_queue_part2();
+  }
+
+  if (artist_name_from_above == "coldpla_songs") {
+    clean_queue_part1();
+    songs = coldplay_songs;
+    clean_queue_part2();
+  }
+  if (artist_name_from_above == "neffe_songs") {
+    clean_queue_part1();
+    songs = neffex_songs;
+    clean_queue_part2();
+  }
+  if (artist_name_from_above == "e_songs") {
+    clean_queue_part1();
+    songs = ed_songs;
+    clean_queue_part2();
+  }
+  if (artist_name_from_above == "arian_songs") {
+    clean_queue_part1();
+    songs = ariana_songs;
+    clean_queue_part2();
+  }
+  if (artist_name_from_above == "ann_songs") {
+    clean_queue_part1();
+    songs = anne_songs;
+    clean_queue_part2();
+  }
+  if (artist_name_from_above == "camil_songs") {
+    clean_queue_part1();
+    songs = camila_songs;
+    clean_queue_part2();
+  }
+  if (artist_name_from_above == "elli_songs") {
+    clean_queue_part1();
+    songs = ellie_songs;
+    clean_queue_part2();
+  }
+  if (artist_name_from_above == "emine_songs") {
+    clean_queue_part1();
+    songs = eminem_songs;
+    clean_queue_part2();
+  }
+  if (artist_name_from_above == "mi_songs") {
+    clean_queue_part1();
+    songs = mix_songs;
+    clean_queue_part2();
+  }
+}
